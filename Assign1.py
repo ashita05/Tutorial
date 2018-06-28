0a8ba972402ef6907687109e8b1896c3281301b1
ashita05 <ashitarai1998@gmail.com>
2018-06-29 00:29:27 +0530

    intro to python

---
+++ Assign1.py
@@ -0,0 +1,15 @@
+#write something
+print('Hi! Wade')
+#2. Join strings
+a = 'Dead'
+b = 'Pool'
+print(a+b)
+#3.print variable values
+a = 2.05
+print('Value =',a)
+#5.Placeholders
+print('This is float:%f'%(4.5))
+print('This is float:%2f'%(4.5))
+print('This is float:%7f'%(4.5))
+#6.Fill in the blanks
+input("My name is ")
